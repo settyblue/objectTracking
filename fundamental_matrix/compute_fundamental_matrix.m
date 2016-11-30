a35590fa25ab7e67468e89ea15d6a43631b72869
swaminathan.42@osu.edu <swaminathan.42@osu.edu>
2016-11-30 16:17:52 -0500

    syncing code, ignore png files

--- fundamental_matrix/compute_fundamental_matrix.m
+++ fundamental_matrix/compute_fundamental_matrix.m
@@ -1,7 +1,9 @@
 function [fundamental_matrix] = compute_fundamental_matrix (image1_points, image2_points)
     [norm_img_1_points, img_1_trans_mat] = transform_points(image1_points);
     [norm_img_2_points, img2_trans_mat] = transform_points(image2_points);
-    matrix_a = [norm_img_1_points .* norm_img_2_points(:, 1) norm_img_1_points .* norm_img_2_points(:, 2) norm_img_1_points];
+    %size(norm_img_1_points)
+    %size(norm_img_2_points)
+    matrix_a = [norm_img_1_points(:, 1) .* norm_img_2_points(:, 1) norm_img_1_points(:, 2) .* norm_img_2_points(:, 1) norm_img_1_points(:, 3) .* norm_img_2_points(:, 1) norm_img_1_points(:, 1) .* norm_img_2_points(:, 2) norm_img_1_points(:, 2) .* norm_img_2_points(:, 2) norm_img_1_points(:, 3) .* norm_img_2_points(:, 2) norm_img_1_points];
     [eig_vectors, eig_values] = eig(matrix_a' * matrix_a);
     norm_funda_matrix = reshape(eig_vectors(:, diag(eig_values) ==  min(diag(eig_values))), 3, 3);
     norm_funda_matrix = norm_funda_matrix';
