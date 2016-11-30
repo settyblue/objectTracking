bc3180c1289024293cc5ad9c77fd09225127447a
Rakshith Kunchum <settyblue@gmail.com>
2016-11-29 17:29:37 -0800

    Added depth funciton. not tested.

---
+++ mean_tracking/get_depth.m
@@ -0,0 +1,24 @@
+function [ depth ] = get_depth( tracking_position, tracking_object_position_r, tracking_object_size )
+%GET_DEPTH Summary of this function goes here
+%   Detailed explanation goes here
+    % tracking_point_row =  tracking_position(1,2) + tracking_object_size(1,2)/2;
+    % object center is passed here.
+    middle_column = 512;
+    xl =  tracking_position(1,1) - middle_column;
+    xr =  middle_column - tracking_object_position_r(1,1);
+    
+    simple_depth = 1.0/(xl-xr);
+    depth = simple_depth;
+    
+    theta = 80;
+    fl = 0.03; % focal_length left camera
+    fr = 0.03; % focal_length right camera
+    f = fr;
+    T = 0.1; %distance between the camera
+    xld = fl*xl/sqrt(fl*fl + xl*xl)/(sind(theta - atand(xl/fl)));
+    xrd = fr*xr/sqrt(fr*fr + xr*xr)/(sind(theta - atand(xr/fr)));
+    
+    complex_depth = f*(sind(theta))*(T-2*f*cosd(theta)-xld-xlr)/(2*f*cosd(theta)+xld+xlr) + f*sind(theta);
+    depth = complex_depth;
+end
+
